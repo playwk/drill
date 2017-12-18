6a59ba0134745285065e319d00e51afdca2d387c
tony <sun_jzz@163.com>
2017-12-18 18:06:04 +0800

    update

--- demo/mysql.py
+++ demo/mysql.py
@@ -2,9 +2,9 @@
 # @author ZhengZhong,Jiang
 # @time 2017/12/12 19:54
 
-# import sys
-# reload(sys)
-# sys.setdefaultencoding('utf-8')
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 import os
 import datetime
@@ -41,7 +41,7 @@ class Check:
 
         try:
             self.host = config.get(self.role, 'host')
-            self.port = config.get(self.role, 'port')
+            self.port = int(config.get(self.role, 'port'))
             self.user = config.get(self.role, 'user')
             self.password = config.get(self.role, 'password')
             self.data_vol = config.get(self.role, 'data_vol')
@@ -54,25 +54,21 @@ class Check:
             print('获取状态值失败!')
 
     def syncstatus(self):
-        status = False
+        status = u'不正常'
         try:
-            conn = pymysql.connect(self.host, self.port,
-                                   self.user, self.password
-                                   )
+            conn = pymysql.connect(host=self.host, port=self.port, user=self.user, password=self.password)
             cur = conn.cursor()
-            cur.execute('show slave status')
+            cur.execute('show slave status;')
             for n in cur.fetchall():
                 if n[10] == n[11] == 'Yes':
-                    status = True
-            cur.close()
-            conn.close()
+                    status = u'正常'
         except pymysql.Error as e:
             print("pymysql Error!", e)
         return status
 
     def data_vol_used(self):
         data_vol_used = psutil.disk_usage(self.data_vol).percent
-        return data_vol_used
+        return "%s %s%%" % (self.data_vol, data_vol_used)
 
     def status(self):
         res = Popen("mysql -h%s -P%s -u%s -p%s -e'status'" % (self.host,
@@ -93,32 +89,37 @@ class Check:
         return res[-3].strip("\n")
 
     def data_size(self):
-        res = Popen("mysql -h%s -P%s -u%s -p%s -e'SELECT concat(truncate((sum(DATA_LENGTH)+sum(INDEX_LENGTH))/1024/1024, 2), 'MB') as data_size FROM information_schema.TABLES where TABLE_SCHEMA=%s'"
-                  % (self.host, self.port,
-                     self.user, self.password,
-                     self.check_dbs), shell=True, stdout=PIPE)
-        dbsize = Popen("sed -n '2p'", shell=True, stdin=res.stdout, stdout=PIPE)
-        return dbsize
+        try:
+            conn = pymysql.connect(host=self.host, port=self.port,
+                                   user=self.user, password=self.password)
+            cur = conn.cursor()
+            cur.execute("SELECT concat(truncate((sum(DATA_LENGTH)+sum(INDEX_LENGTH))/1024/1024, 2), 'MB') as data_size FROM information_schema.TABLES where TABLE_SCHEMA='%s'" % self.check_dbs)
+            return cur.fetchall()[0][0]
+        except pymysql.Error as e:
+            return "pymysql Error! %s" % e
 
     def conn(self):
         try:
-            conn = pymysql.connect(self.host, self.port,
-                                   self.user, self.password
+            conn_list = []
+            conn = pymysql.connect(host=self.host, port=self.port,
+                                   user=self.user, password=self.password
                                    )
             cur = conn.cursor()
             cur.execute("show processlist;")
             current_conn = len(cur.fetchall())
+            conn_list.append(current_conn)
             cur.execute("show status like 'Max_used_connections';")
             max_used_conn = cur.fetchall()[0][1]
+            conn_list.append(max_used_conn)
             cur.close()
             conn.close()
-            return current_conn, max_used_conn
+            return conn_list 
         except pymysql.Error as e:
             print("pymysql Error!", e)
             return False
 
     def log_alarm(self):
-        log_filter = Popen("grep %s %s".format(nowtime, self.errlog_path), shell=True,
+        log_filter = Popen("grep %s %s" % (nowtime, self.errlog_path), shell=True,
                           stdout=PIPE)
         log_alarm = Popen("grep -iw '\[error\]'", shell=True, stdin=log_filter.stdout, stdout=PIPE)
         if not log_alarm:
@@ -126,10 +127,10 @@ class Check:
         else:
             return u'有告警'
 
-    def bak_vol(self):
+    def bakup_status(self):
         os.path.exists("%s/%s" % (self.bak_vol, 'name'))
         bak_vol_used = psutil.disk_usage(self.bak_vol).percent
-        return bak_vol_used
+        return  "%s %s%%" % (self.bak_vol, bak_vol_used)
 
     def mycat_status(self):
         for pid in psutil.pids():
@@ -138,40 +139,6 @@ class Check:
                 return u'正常'
             else:
                 return u'不正常'
-
-    # def get_value(self):
-    #     check_res = {}
-    #     mysql_status = Popen("mysql -u%s -p%s -e'status'".format(self.user, self.password),
-    #                          shell=True, stdout=PIPE)
-    #     uptime = Popen("grep '^Uptime'", shell=True, stdin=mysql_status.stdout, stdout=PIPE)
-    #     basestatus = Popen("grep '^Threads'", shell=True, stdin=mysql_status.stdout, stdout=PIPE)
-    #
-    #     conn_status = Popen("mysql -h% -P%s -u%s -p%s -e'show status like '%conn%''" % (self.host, self.port, self.user, self.password),
-    #                          shell=True, stdout=PIPE)
-    #     current_conn = Popen("awk '/Threads_connected/{print $2}'", shell=True, stdin=conn_status.stdout, stdout=PIPE)
-    #     max_used_conn = Popen("awk '/Max_used_connections/{print $2}'", shell=True, stdin=conn_status.stdout, stdout=PIPE)
-    #
-    #     log_filter = Popen("grep %s %s".format(nowtime, check_category['errlog_path']), shell=True,
-    #                       stdout=PIPE)
-    #     log_alarm = Popen("grep -iw '\[error\]'", shell=True, stdin=log_filter.stdout, stdout=PIPE)
-    #     if not log_alarm:
-    #         log_alarm = u'无告警'
-    #     else:
-    #         log_alarm = u'有告警'
-    #
-    #     check_res['data_vol_used'] = data_vol_used
-    #     check_res['uptime'] = uptime
-    #     check_res['basestatus'] = basestatus
-    #     check_res['current_conn'] = current_conn
-    #     check_res['max_used_conn'] = max_used_conn
-    #     check_res['log_alarm'] = log_alarm
-    #     check_res['dbsize'] = dbsize
-    #
-    #     if self.role == "slave":
-    #         check_res['sync_status'] = self.syncstatus()
-    #     elif self.role == "master":
-    #         check_res['db_bak'] =
-    #     return check_res
 
     def copy_new_sheet(self):
         rb = xlrd.open_workbook('mmc_db_check.xls', formatting_info=True)
@@ -192,15 +159,15 @@ class Check:
         wb = copy(rb)
         ws = wb.get_sheet(-1)
         if self.role == 'master':
-            ws.write(1, 1, self.data_vol, style)
+            ws.write(1, 1, self.data_vol_used(), style)
             ws.write(2, 1, self.basestatus(), style)
             ws.write(3, 1, self.uptime(), style)
             ws.write(4, 1, self.data_size(), style)
             ws.write(5, 1, self.conn()[0], style)
             ws.write(6, 1, self.conn()[1], style)
-            ws.write(8, 1, self.log_alarm(), style)
-            ws.write(9, 1, self.bak_vol, style)
-            ws.write(10, 1, self.mycat_status(), style)
+            ws.write(7, 1, self.log_alarm(), style)
+            ws.write(8, 1, self.bak_vol, style)
+            ws.write(9, 1, self.mycat_status(), style)
         elif self.role == 'slave':
             ws.write(1, 2, self.data_vol, style)
             ws.write(2, 2, self.basestatus(), style)
@@ -217,6 +184,13 @@ if __name__ == '__main__':
     master_check = Check('master')
     print(master_check.uptime())
     print(master_check.basestatus())
+    print(master_check.data_vol_used())
+    print(master_check.data_size())
+    print(master_check.conn()[0])
+    print(master_check.conn()[1])
+    print(master_check.log_alarm())
+    print(master_check.bakup_status())
+    print(master_check.mycat_status())
     master_check.copy_new_sheet()
     master_check.put_new_value()
     slave_check = Check('slave')
