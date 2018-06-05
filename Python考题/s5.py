7322407dc50d5568dfb286289171dca6702a8046
tony <sun_jzz@163.com>
2018-06-05 14:43:42 +0800

    update

--- Python考题/s5.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# @author ZhengZhong,Jiang
-# @time 2018/4/4 16:33
-
-import smtplib
--- Python考题/s6.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# @author ZhengZhong,Jiang
-# @time 2018/4/4 16:59
-
-import os
-
-def send_ping(arg):
-    target= arg
-    
